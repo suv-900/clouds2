e22dd328b62ffdf85b493182afd6c683fdfcc050
suv-900 <suvhampaul36@gmail.com>
2024-07-04 16:22:50 +0530

    routing changed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,35 @@
 import React from 'react';
-import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+import {BrowserRouter, Route,Routes} from 'react-router-dom';
 import {PostViewer} from './components/posts/PostViewer';
 import Login from './components/user/Login';
 import Register from './components/user/Register';
 import Home from './components/Home';
 import CreatePost from './components/user/CreatePost';
 import ErrorPage from './components/errors/ErrorPage';
-import Loading from './components/Loading';
-
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element:<h2>Main Page</h2>
-  },
-  {
-    path:"/login",
-    element:<Login/>
-  },
-  {
-    path:"/register",
-    element:<Register/>
-  },
-  {
-    path:"/home",
-    element:<Home/>
-  },
-  {
-    path:"/post/view",
-    element:<PostViewer />
-  },
-  {
-    path:"/createpost",
-    element:<CreatePost/>
-  },
-  {
-    path:"/error",
-    element:<ErrorPage/>
-  }
-])
+import { ProtectRoutes } from './components/ProtectRoutes';
+import UserInfo from './components/user/UserInfo';
 
 function App() {
   return (
   <div>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/v" element={<ProtectRoutes/>}>
+          <Route path="/v/createpost" element={<CreatePost/>}/>
+        </Route>
+
+        <Route path="/" element={<h1>Main Page</h1>}/>
+
+        <Route path="/login" element={<Login/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/register" element={<Register/>} />
+
+        <Route path="/posts/view" element={<PostViewer/>} />
+
+        <Route path="/user/:username" element={<UserInfo/>}/>
+        <Route path="/error" element={<ErrorPage/>}/>
+      </Routes>
+    </BrowserRouter>
   </div> 
   );
 }
