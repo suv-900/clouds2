7cadf7f643ecf4a6603137c4b5e67c1628ec62ba
suv-900 <suvhampaul36@gmail.com>
2024-07-29 16:22:12 +0530

    updated registry

--- server/internal/data/models.go
+++ server/internal/data/models.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"gorm.io/gorm"
 )
@@ -13,6 +15,7 @@ var (
 	ErrConflict            = errors.New("already exists")
 	ErrRecordNotFound      = errors.New("not found")
 	ErrInternalServerError = errors.New("internal server error")
+	ErrIllegalState        = errors.New("illegal state")
 )
 
 const context_timeout = 5 * time.Second
@@ -27,10 +30,22 @@ type Models struct {
 		CheckUserExists(cx context.Context, username string) (bool, error)
 		GetUserPassword(cx context.Context, username string) (string, error)
 	}
+	Posts interface {
+		AddPost(cx context.Context, post *Post) (int, error)
+		GetPost(cx context.Context, postID primitive.ObjectID) (Post, error)
+		UpdatePost(cx context.Context, update bson.D, postID primitive.ObjectID) error
+		DeletePost(cx context.Context, postID primitive.ObjectID) error
+
+		CheckPostTitleExists(cx context.Context, post_title string) (bool, error)
+		GetPostsByAuthorID(cx context.Context, authorID int64, offset int64) ([]Post, error)
+		IncrementLike(cx context.Context, postID primitive.ObjectID) error
+		DecrementLike(cx context.Context, postID primitive.ObjectID) error
+	}
 }
 
 func GetModels(gd *gorm.DB, mc *mongo.Client) Models {
 	return Models{
 		Users: UserClient{db: gd},
+		Posts: PostClient{client: mc},
 	}
 }
